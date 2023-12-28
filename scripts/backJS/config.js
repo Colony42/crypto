f789c784a0cbd7ff85de8704731288a956042fee
Colony42 <147247688+Colony42@users.noreply.github.com>
2023-12-28 23:03:17 +0300

    Update config.js

--- scripts/backJS/config.js
+++ scripts/backJS/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-   host : 'sql213.infinityfree.com',
-   user : 'if0_35691160',
-   password : 'VK2004on',
-   database : 'if0_35691160_db_cry'
+   host : 'bwululgm8l4lay4dbpen-mysql.services.clever-cloud.com',
+   user : 'uibm4rkkqjnmqkkn',
+   password : 'g6xYPjIXjNdff46JQqPw',
+   database : 'bwululgm8l4lay4dbpen'
 }
